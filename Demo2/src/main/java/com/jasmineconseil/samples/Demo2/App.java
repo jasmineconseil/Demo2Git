cfbd067e45581b1ca69bace6876cdb8fb98bc499
Karim DJAAFAR <kdjaafar@jasmineconseil.com>
2014-02-02 18:52:30 +0100

    First Commit

---
+++ Demo2/src/main/java/com/jasmineconseil/samples/Demo2/App.java
@@ -0,0 +1,13 @@
+package com.jasmineconseil.samples.Demo2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
